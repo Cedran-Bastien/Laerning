bb670220d931cfeb6cb5002fc5c5df28cf360b3b
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2023-11-30 18:02:52 +0100

    Correct bug and configuration
    to rebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    extends: [
-      'airbnb', 
-      'airbnb/hooks',
-      'plugin:react/jsx-runtime'
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/jsx-runtime",
+    "next/core-web-vitals",
+  ],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+  },
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"],
+      },
     ],
-    parserOptions: {
-      ecmaVersion: 8,
-      sourceType: "module",
-    },
-    rules: {
-      'react/jsx-filename-extension': [
-        1,
-        { 
-          "extensions": [
-            ".js",
-            ".jsx",
-          ] 
-        }
-      ],
-      'react/jsx-props-no-spreading' : 'off',
-      'react/jsx-no-constructed-context-values': 'off',
-      'max-len': 'off'
-    },
-    
-  };
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-no-constructed-context-values": "off",
+    "max-len": "off",
+    "import/no-unresolved": "off",
+  },
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <p className="ml-4">Layout</p>;
+}
