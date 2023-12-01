0ca4b26fa19ff1c0c37885f833f590520aa43b7c
zhisheng <zhishengtrieu@gmail.com>
2023-12-01 11:50:23 +0100

    feat(learning) : add grid and img

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+
+
+const CardGrid = ({ names }) => {
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-2xl">
+        {names.map((name, index) => (
+          <div key={index} className="w-full h-auto bg-white rounded overflow-hidden shadow-lg">
+            <Image 
+              src={`/img/${name}.png`} 
+              width="320" 
+              height="380" 
+              layout="responsive" 
+              alt={name} 
+              className="object-cover object-center w-full h-full"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardGrid;
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -1,3 +1,11 @@
+import CardGrid from "@/components/CardGrid";
+
 export default function Learning() {
-  return <p>learn</p>;
+  const names = ["pig", "cow", "chicken", "sheep", "horse"];
+  return (
+    <div>
+      <h1 className="text-4xl text-center">Welcome to my farm!</h1>
+      <CardGrid names={names} />
+    </div>
+  );
 }
