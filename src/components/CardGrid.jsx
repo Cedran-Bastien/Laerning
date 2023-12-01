b47ef9a56b8c271a88f904d6cf95bef9d09c8f78
zhisheng <zhishengtrieu@gmail.com>
2023-12-01 15:43:48 +0100

    feat(langue): prise en charge de plusieurs langues

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 
 
-const CardGrid = ({ names }) => {
+const CardGrid = ({ imgNames, names }) => {
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-2xl">
         {names.map((name, index) => (
           <div key={index} className="w-full h-auto bg-white rounded overflow-hidden shadow-lg">
             <Image 
-              src={`/img/${name}.png`} 
+              src={`/img/${imgNames[index]}.png`} 
               width="320" 
               height="380" 
               layout="responsive" 
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
 import CardGrid from "@/components/CardGrid";
+//on recupere les langues des animaux dans un tableau depuis le dossier data
+// on importe tous de set1
+import * as set1 from "@/data/set1";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default function Learning() {
-  const names = ["pig", "cow", "chicken", "sheep", "horse"];
+  const [selectedLanguage, setSelectedLanguage] = useState('en');
+  const langues = Object.keys(set1);
+  const handleLanguageChange = (event, value) => {
+    setSelectedLanguage(value);
+  };
+
   return (
     <div>
       <h1 className="text-4xl text-center">Welcome to my farm!</h1>
-      <CardGrid names={names} />
+      <p className="text-center">{}</p>
+      <div className="flex justify-center m-4">
+        <Autocomplete
+          disablePortal
+          id="combo-box-langues"
+          className=""
+          options={langues}
+          sx={{ width: 300 }}
+          value={selectedLanguage}
+          onChange={handleLanguageChange}
+          renderInput={(params) => <TextField {...params} label="Langues" />}
+        />
+      </div>
+      <h1 className="text-2xl text-center">Learn the names of the animals in {selectedLanguage}</h1>
+      <CardGrid imgNames={set1.en} names={set1[selectedLanguage]} />
     </div>
   );
 }
